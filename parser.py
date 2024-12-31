b848b2d376cc6c519ee347a8df54d5090e3fa671
gh-nate <122752052+gh-nate@users.noreply.github.com>
2024-12-30 16:37:30 -0800

    Chapter 4: The Parser

--- parser.py
+++ parser.py
@@ -23,6 +23,14 @@ PRECEDENCE = {
     "%": 50,
     "+": 45,
     "-": 45,
+    "<": 35,
+    "<=": 35,
+    ">": 35,
+    ">=": 35,
+    "==": 30,
+    "!=": 30,
+    "&&": 10,
+    "||": 5,
 }
 
 
@@ -88,7 +96,7 @@ def parse_factor(tokens):
     if lexer.TOKEN_PATTERNS[lexer.Token.CONSTANT].match(next_token):
         take_token(tokens)
         return asdl.ConstantAST(int(next_token))
-    elif next_token in ["~", "-"]:
+    elif next_token in ["~", "-", "!"]:
         return asdl.UnaryAST(
             parse_unop(tokens),
             parse_factor(tokens),
@@ -107,6 +115,8 @@ def parse_unop(tokens):
             return asdl.UnaryOperatorAST.NEGATE
         case "~":
             return asdl.UnaryOperatorAST.COMPLEMENT
+        case "!":
+            return asdl.UnaryOperatorAST.NOT
         case _:
             raise SyntaxError(f"Unknown unary operator: {token}")
 
@@ -123,5 +133,21 @@ def parse_binop(tokens):
             return asdl.BinaryOperatorAST.DIVIDE
         case "%":
             return asdl.BinaryOperatorAST.REMAINDER
+        case "<":
+            return asdl.BinaryOperatorAST.LESS_THAN
+        case "<=":
+            return asdl.BinaryOperatorAST.LESS_OR_EQUAL
+        case ">":
+            return asdl.BinaryOperatorAST.GREATER_THAN
+        case ">=":
+            return asdl.BinaryOperatorAST.GREATER_OR_EQUAL
+        case "==":
+            return asdl.BinaryOperatorAST.EQUAL
+        case "!=":
+            return asdl.BinaryOperatorAST.NOT_EQUAL
+        case "&&":
+            return asdl.BinaryOperatorAST.AND
+        case "||":
+            return asdl.BinaryOperatorAST.OR
         case _:
             raise SyntaxError(f"Unknown binary operator: {token}")
