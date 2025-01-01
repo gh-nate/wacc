6ad60fd9c1666642279c99787eef4aa2402368a3
gh-nate <122752052+gh-nate@users.noreply.github.com>
2024-12-31 16:17:58 -0800

    Chapter 8: Semantic Analysis

--- semantic_analysis.py
+++ semantic_analysis.py
@@ -18,22 +18,29 @@ from dataclasses import dataclass
 import asdl
 
 
-def _mk_tmp(name):
+def _mk_tmp(prefix):
     count = 0
     while True:
-        yield f"{name}.{count}"
+        yield f"{prefix}{count}"
         count += 1
 
 
 make_temporary = next
+make_label = make_temporary
 
 
 class ResolutionError(Exception):
     pass
 
 
+class LoopLabellingError(Exception):
+    pass
+
+
 def analyze(tree):
-    variable_resolution(tree.function_definition.body)
+    body = tree.function_definition.body
+    variable_resolution(body)
+    loop_label(body)
 
 
 def variable_resolution(body):
@@ -53,6 +60,14 @@ def resolve_block_item(block_item, variable_map):
             resolve_declaration(declaration, variable_map)
 
 
+def resolve_for_init(init, variable_map):
+    match init:
+        case asdl.InitDeclAST(d):
+            resolve_declaration(d, variable_map)
+        case asdl.InitExpAST(e):
+            resolve_exp(e, variable_map)
+
+
 @dataclass
 class MapEntry:
     new_name: str
@@ -67,7 +82,7 @@ def resolve_declaration(declaration, variable_map):
             raise ResolutionError(f"Duplicate variable declaration for {name}!")
         g = v[1]
     else:
-        g = _mk_tmp(name)
+        g = _mk_tmp(name + ".")
     declaration.name = make_temporary(g)
     variable_map[name] = MapEntry(declaration.name, True), g
     resolve_exp(declaration.init, variable_map)
@@ -81,6 +96,18 @@ def resolve_statement(statement, variable_map):
             resolve_exp(condition, variable_map)
             resolve_statement(then, variable_map)
             resolve_statement(else_, variable_map)
+        case asdl.WhileAST(condition, body, _):
+            resolve_exp(condition, variable_map)
+            resolve_statement(body, variable_map)
+        case asdl.DoWhileAST(body, condition, _):
+            resolve_statement(body, variable_map)
+            resolve_exp(condition, variable_map)
+        case asdl.ForAST(init, condition, post, body, _):
+            new_variable_map = copy_variable_map(variable_map)
+            resolve_for_init(init, new_variable_map)
+            resolve_exp(condition, new_variable_map)
+            resolve_exp(post, new_variable_map)
+            resolve_statement(body, new_variable_map)
         case asdl.CompoundAST(block):
             resolve_block(block, copy_variable_map(variable_map))
 
@@ -112,3 +139,43 @@ def copy_variable_map(variable_map):
     for k, (map_entry, g) in variable_map.items():
         new_variable_map[k] = MapEntry(map_entry.new_name, False), g
     return new_variable_map
+
+
+def loop_label(block):
+    label_block({label: _mk_tmp(label) for label in ["w", "d", "f"]}, block)
+
+
+def label_block(g, block, current_label=None):
+    for item in block.items:
+        match item:
+            case asdl.SAST(statement):
+                label_statement(g, statement, current_label)
+
+
+def label_statement(g, statement, current_label=None):
+    match statement:
+        case asdl.IfAST(_, then, else_):
+            label_statement(g, then, current_label)
+            label_statement(g, else_, current_label)
+        case asdl.BreakAST(_):
+            if not current_label:
+                raise LoopLabellingError("break statement outside of loop")
+            statement.label = current_label
+        case asdl.ContinueAST(_):
+            if not current_label:
+                raise LoopLabellingError("continue statement outside of loop")
+            statement.label = current_label
+        case asdl.WhileAST(_, body, _):
+            new_label = make_label(g["w"])
+            label_statement(g, body, new_label)
+            statement.label = new_label
+        case asdl.DoWhileAST(body, _, _):
+            new_label = make_label(g["d"])
+            label_statement(g, body, new_label)
+            statement.label = new_label
+        case asdl.ForAST(_, _, _, body, _):
+            new_label = make_label(g["f"])
+            label_statement(g, body, new_label)
+            statement.label = new_label
+        case asdl.CompoundAST(block):
+            label_block(g, block, current_label)
